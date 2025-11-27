b26bf00d31399edbc70a9665019dbc73fced78bc
riqsans <thrq88@gmail.com>
2025-11-27 13:51:14 +0700

    Menambahkan pemetaan untuk `telpon` di fungsi `toSiswa`

--- app/src/main/java/com/example/datasiswa/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/datasiswa/viewmodel/EntryViewModel.kt
@@ -47,6 +47,7 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     id = id,
     nama = nama,
     alamat = alamat,
+    telpon = telpon
 )
 
 fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
