c64ce047182f19a91995251ec02e39dc086dc12b
riqsans <thrq88@gmail.com>
2025-11-27 13:08:16 +0700

    Menambahkan implementasi `getDatabase` untuk membuat dan mengambil instance database Room.

--- app/src/main/java/com/example/datasiswa/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/datasiswa/room/DatabaseSiswa.kt
@@ -13,3 +13,14 @@ abstract class DatabaseSiswa : RoomDatabase(){
         @Volatile
         private var Instance: DatabaseSiswa? = null
 
+        fun getDatabase(context: Context): DatabaseSiswa{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build()
+                    .also { Instance = it }
+            })
+        }
+    }
+}
