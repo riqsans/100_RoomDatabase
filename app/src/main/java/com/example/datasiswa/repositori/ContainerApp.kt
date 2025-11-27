571181032b1b999f65c665820f859da8e2d2da29
riqsans <thrq88@gmail.com>
2025-11-27 13:19:15 +0700

    Membuat class `ContainerDataApp` yang mengimplementasikan `ContainerApp`

--- app/src/main/java/com/example/datasiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/datasiswa/repositori/ContainerApp.kt
@@ -1,8 +1,14 @@
 package com.example.datasiswa.repositori
 
+import android.content.Context
+import com.example.datasiswa.room.DatabaseSiswa
+
 interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
 }
 
-
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
 }
