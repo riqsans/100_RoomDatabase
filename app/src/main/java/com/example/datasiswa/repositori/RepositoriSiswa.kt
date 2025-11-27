31cc8417aad015d1e878661d85294350a11ef1da
riqsans <thrq88@gmail.com>
2025-11-27 13:11:46 +0700

    Menambahkan import untuk Flow dan Siswa di RepositoriSiswa

--- app/src/main/java/com/example/datasiswa/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/datasiswa/repositori/RepositoriSiswa.kt
@@ -1,5 +1,8 @@
 package com.example.datasiswa.repositori
 
+import com.example.datasiswa.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
