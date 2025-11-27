775a0682259003a4dd23fc1b09717e8c9528a746
riqsans <thrq88@gmail.com>
2025-11-27 14:05:04 +0700

    Membuat `PenyediaViewModel` untuk menyediakan `Factory` bagi `HomeViewModel` dan `EntryViewModel`

---
+++ app/src/main/java/com/example/datasiswa/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.datasiswa.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.datasiswa.repositori.AplikasiSiswa
+import com.example.datasiswa.viewmodel.EntryViewModel
+import com.example.datasiswa.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory{
+        initializer {
+            HomeViewModel(aplikasiSiswa().containerApp.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().containerApp.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari AplikasiSiswa
+*/
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
