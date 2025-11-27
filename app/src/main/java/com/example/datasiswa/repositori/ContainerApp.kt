d985894677ff941a5b2cc334f1b3aecc7b6eefe4
riqsans <thrq88@gmail.com>
2025-11-27 13:21:15 +0700

    Membuat class `AplikasiSiswa` yang meng-extend `Application` untuk inisialisasi container dependensi.

--- app/src/main/java/com/example/datasiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/datasiswa/repositori/ContainerApp.kt
@@ -1,5 +1,6 @@
 package com.example.datasiswa.repositori
 
+import android.app.Application
 import android.content.Context
 import com.example.datasiswa.room.DatabaseSiswa
 
@@ -10,5 +11,16 @@ interface ContainerApp{
 class ContainerDataApp(private val context: Context): ContainerApp{
     override val repositoriSiswa: RepositoriSiswa by lazy {
         OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+class AplikasiSiswa: Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var containerApp: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerDataApp(this)
     }
 }
