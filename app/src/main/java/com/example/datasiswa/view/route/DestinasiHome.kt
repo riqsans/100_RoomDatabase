e5b2f5f807b2bf0eaad4a4a7a32975a4b6296d9a
riqsans <thrq88@gmail.com>
2025-11-27 13:33:06 +0700

    Membuat object `DestinasiHome` untuk navigasi halaman utama

---
+++ app/src/main/java/com/example/datasiswa/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase.view.route
+
+import com.example.datasiswa.R
+import com.example.datasiswa.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
