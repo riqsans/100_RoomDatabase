ed1ee6ebf4fd2da0fde5ba59a6e162fdd175b8da
riqsans <thrq88@gmail.com>
2025-11-27 13:17:31 +0700

    Merapikan kode dengan menghapus tanda kurung siku di anotasi `@Database`

--- app/src/main/java/com/example/datasiswa/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/datasiswa/room/DatabaseSiswa.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Siswa::class], version = 1, exportSchema = false)])
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase(){
     abstract fun siswaDao(): SiswaDao
 
