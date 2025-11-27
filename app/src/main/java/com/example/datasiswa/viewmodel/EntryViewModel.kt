87914610af1a8dc7193b0cc519ae563071d56154
riqsans <thrq88@gmail.com>
2025-11-27 13:40:36 +0700

    - Menambahkan fungsi `updateUiState` dan `saveSiswa` di `EntryViewModel`. - `updateUiState` digunakan untuk memperbarui `uiStateSiswa` dan memvalidasi input. - `saveSiswa` digunakan untuk menyimpan data siswa ke repositori setelah validasi.

--- app/src/main/java/com/example/datasiswa/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/datasiswa/viewmodel/EntryViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.datasiswa.viewmodel
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.datasiswa.repositori.RepositoriSiswa
 
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
 
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
@@ -12,3 +13,15 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    suspend fun  saveSiswa(){
+        if (validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
