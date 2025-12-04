49b212fbe30a67c27ce42b0d8c7879cad79683de
riqsans <thrq88@gmail.com>
2025-12-04 12:24:54 +0700

    Menambahkan fungsi `getSiswaStream` dan `deleteSiswa` di `RepositoriSiswa`

--- app/src/main/java/com/example/datasiswa/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/datasiswa/repositori/RepositoriSiswa.kt
@@ -6,13 +6,29 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+
     suspend fun insertSiswa(siswa: Siswa)
+
 }
 
-class OfflineRepositoriSiswa(private val siswaDao: SiswaDao): RepositoriSiswa{
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa{
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
+    //override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 
 }
+
 
